28fdfc50c04c706e33922d9bf5fbe0241ec02ac5
davecoders0201 <davecoder0201@gmail.com>
2023-04-13 17:02:53 +0530

    Changed string from number of id

--- data/tutorApiAhemdabad.js
+++ data/tutorApiAhemdabad.js
@@ -1,18 +1,18 @@
 const Courses = [
   {
-    id: '1',
+    id: 1,
     title: 'Demo 1',
     image: require('../asset/web.jpg'),
     description: 'Maths, Science, SS ',
   },
   {
-    id: '2',
+    id: 2,
     title: 'Demo 2 ',
     image: require('../asset/graphic.jpg'),
     description: 'Gujarati, English',
   },
   {
-    id: '3',
+    id: 3,
     title: 'Demo 3',
     image: require('../asset/coding.jpg'),
     description: 'PT, Hindi',
