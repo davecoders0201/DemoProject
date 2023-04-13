a51d0acfbeb7994e8d433d0300ca21ee8dab2ebe
davecoders0201 <davecoder0201@gmail.com>
2023-04-13 17:04:08 +0530

    Changes string to number and remove image

--- data/tutorApi.js
+++ data/tutorApi.js
@@ -2,19 +2,16 @@ const Courses = [
   {
     id: '1',
     title: 'Demo 4',
-    image: require('../asset/web.jpg'),
     description: 'Maths, Science, SS ',
   },
   {
     id: '2',
     title: 'Demo 5',
-    image: require('../asset/graphic.jpg'),
     description: 'Gujarati, English',
   },
   {
     id: '3',
     title: 'Demo 6',
-    image: require('../asset/coding.jpg'),
     description: 'PT, Hindi',
   },
 ];
