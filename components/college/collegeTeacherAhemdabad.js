bc118cbab305716ed9dc4c4823b86c83297abfe3
davecoders0201 <davecoder0201@gmail.com>
2023-04-14 17:32:43 +0530

    Map function added

--- components/college/collegeTeacherAhemdabad.js
+++ components/college/collegeTeacherAhemdabad.js
@@ -9,73 +9,76 @@ const CollegeTeacherAhemdabad = ({navigation}) => {
   const [intervalId, setIntervalId] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const courseCard = ({item}) => {
-    const startTimer = () => {
-      if (!isPlaying) {
-        const id = setInterval(() => {
-          setTimer(prevTimer => prevTimer + 1);
-        }, 1000);
-        setIntervalId(id);
-        setIsPlaying(true);
-      }
-    };
+  // const courseCard = ({item}) => {
+  const startTimer = () => {
+    if (!isPlaying) {
+      const id = setInterval(() => {
+        setTimer(prevTimer => prevTimer + 1);
+      }, 1000);
+      setIntervalId(id);
+      setIsPlaying(true);
+    }
+  };
 
-    const stopTimer = () => {
-      if (isPlaying) {
-        clearInterval(intervalId);
-        setIsPlaying(false);
-      }
-    };
-    const formatTime = time => {
-      const minutes = Math.floor(time / 60)
-        .toString()
-        .padStart(2, '0');
-      const seconds = (time % 60).toString().padStart(2, '0');
-      return `${minutes}:${seconds}`;
-    };
-    return (
-      <View style={{margin: 10}}>
-        <View
+  const stopTimer = () => {
+    if (isPlaying) {
+      clearInterval(intervalId);
+      setIsPlaying(false);
+    }
+  };
+  const formatTime = time => {
+    const minutes = Math.floor(time / 60)
+      .toString()
+      .padStart(2, '0');
+    const seconds = (time % 60).toString().padStart(2, '0');
+    return `${minutes}:${seconds}`;
+  };
+  const selectedCard = tutorApiAhemdabad.find(Id => {
+    return Id.id;
+  });
+  return (
+    <View style={{margin: 10}}>
+      <View
+        style={{
+          backgroundColor: '#ff5435',
+          padding: 10,
+          borderRadius: 10,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <Text
           style={{
-            backgroundColor: '#ff5435',
-            padding: 10,
-            borderRadius: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
+            fontWeight: 'bold',
+            fontSize: 18,
           }}>
-          <Text
-            style={{
-              fontWeight: 'bold',
-              fontSize: 18,
-            }}>
-            {item.title}
+          {selectedCard.title}
+        </Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{marginRight: 10, fontSize: 17}}>
+            {formatTime(timer)}
           </Text>
-          <View style={{flexDirection: 'row'}}>
-            <Text style={{marginRight: 10, fontSize: 17}}>
-              {formatTime(timer)}
-            </Text>
-            <TouchableOpacity onPress={isPlaying ? stopTimer : startTimer}>
-              <Image
-                source={
-                  isPlaying
-                    ? require('../../asset/paus.png')
-                    : require('../../asset/play.png')
-                }
-                style={{width: 20, height: 23}}
-              />
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity onPress={isPlaying ? stopTimer : startTimer}>
+            <Image
+              source={
+                isPlaying
+                  ? require('../../asset/paus.png')
+                  : require('../../asset/play.png')
+              }
+              style={{width: 20, height: 23}}
+            />
+          </TouchableOpacity>
         </View>
       </View>
-    );
-  };
-  return (
-    // Flatlist is a Prop which is use for the Scrolling the Items
-    <FlatList
-      keyExtractor={item => item.id}
-      data={tutorApiAhemdabad}
-      renderItem={courseCard}
-    />
+    </View>
   );
+  // };
+  // return (
+  //   // Flatlist is a Prop which is use for the Scrolling the Items
+  //   <FlatList
+  //     keyExtractor={item => item.id}
+  //     data={tutorApiAhemdabad}
+  //     renderItem={courseCard}
+  //   />
+  // );
 };
 export default CollegeTeacherAhemdabad;
