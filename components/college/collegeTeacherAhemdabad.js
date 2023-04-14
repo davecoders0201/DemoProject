101fe8b673c2431a5da6b830718f27ff1254836c
davecoders0201 <davecoder0201@gmail.com>
2023-04-14 17:04:21 +0530

    Stylesheet removed but function update pending

--- components/college/collegeTeacherAhemdabad.js
+++ components/college/collegeTeacherAhemdabad.js
@@ -1,12 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Image, TouchableOpacity, FlatList} from 'react-native';
 
 // import the Course Api which is created in the API folder
 import tutorApiAhemdabad from '../../data/tutorApiAhemdabad';
@@ -85,66 +78,4 @@ const CollegeTeacherAhemdabad = ({navigation}) => {
     />
   );
 };
-
-// This is One type of css in the React Native File
-const styles = StyleSheet.create({
-  // cardImage: {
-  //   width: '100%',
-  //   height: undefined,
-  //   aspectRatio: 1,
-  // },
-  // mainContainer: {
-  //   paddingHorizontal: 20,
-  // },
-  // courseContainer: {
-  //   padding: 10,
-  //   backgroundColor: 'rgba(255, 255, 255, 0.90)',
-  //   textAlign: 'center',
-  //   borderRadius: 10,
-  //   shadowColor: 'grey',
-  //   shadowOffset: {width: 0, height: 0},
-  //   shadowOpacity: 0.5,
-  //   shadowRadius: 8,
-  //   elevation: 8,
-  //   marginVertical: 10,
-  // },
-  // mainHeader: {
-  //   fontSize: 22,
-  //   color: '#344055',
-  //   textTransform: 'uppercase',
-  //   // fontWeight: 500,
-  //   paddingBottom: 15,
-  //   textAlign: 'center',
-  //   fontFamily: 'Nunito_700Bold',
-  // },
-  // description: {
-  //   textAlign: 'center',
-  //   fontFamily: 'WorkSans_400Regular',
-  //   paddingBottom: 15,
-  //   lineHeight: 20,
-  //   fontSize: 16,
-  //   color: '#7d7d7d',
-  // },
-  // buttonContainer: {
-  //   display: 'flex',
-  //   flexDirection: 'row',
-  //   justifyContent: 'center',
-  // },
-  // buttonStyle: {
-  //   backgroundColor: '#ff5435',
-  //   borderRadius: 5,
-  //   paddingVertical: 10,
-  //   paddingHorizontal: 20,
-  //   display: 'flex',
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  // },
-  // buttonText: {
-  //   fontSize: 20,
-  //   color: '#eee',
-  //   fontFamily: 'WorkSans_400Regular',
-  //   textTransform: 'capitalize',
-  // },
-});
-
 export default CollegeTeacherAhemdabad;
